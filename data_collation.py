8260a538989ca17c642b7c2c457cf464d12272cd
darker66678 <darker66678@gmail.com>
2021-12-11 14:35:15 +0800

    improve code style

--- data_collation.py
+++ data_collation.py
@@ -2,30 +2,30 @@ import os
 import shutil
 
 # deal with train_data
-path=r'./nucleus/train/'
-train_imgs=os.listdir(path)
-train_destination=r'./mmdetection/nucleus/imgs/train/'
+path = r'./nucleus/train/'
+train_imgs = os.listdir(path)
+train_destination = r'./mmdetection/nucleus/imgs/train/'
 
 if not os.path.isdir(train_destination):
     os.makedirs(train_destination)
 
 for img in train_imgs:
-    file=img+'.png'
-    shutil.copy(path+img+"/images/"+file,train_destination+file)
+    file = img+'.png'
+    shutil.copy(path+img+"/images/"+file, train_destination+file)
 
 # deal with test_data
-path=r'./nucleus/test/'
-test_destination=r'./mmdetection/nucleus/imgs/test/'
+path = r'./nucleus/test/'
+test_destination = r'./mmdetection/nucleus/imgs/test/'
 
 if not os.path.isdir(test_destination):
     os.makedirs(test_destination)
 
 for img in os.listdir(path):
-    
-    if img[-3:] =="png":
-        shutil.copy(path+img,test_destination+img)
+
+    if img[-3:] == "png":
+        shutil.copy(path+img, test_destination+img)
 
 # deal with test.json
-shutil.copy(r"./nucleus/test_img_ids.json",r'./mmdetection/nucleus/')
+shutil.copy(r"./nucleus/test_img_ids.json", r'./mmdetection/nucleus/')
 
 print("collation finish!")
